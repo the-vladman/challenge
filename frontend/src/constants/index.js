ee4e1c3741be8abce1b1d1756dfcafb65bab86d9
Vladimir Galicia <thewakosmanclub@gmail.com>
2020-01-07 17:46:16 -0600

    set Constants

---
+++ frontend/src/constants/index.js
@@ -0,0 +1,14 @@
+export default {
+  FTT: 0.0638,
+  interestRate: 0.0234,
+  veiculo: {
+    minLoan: 3000.0,
+    maxLoan: 100000.0,
+    terms: [24, 36, 48]
+  },
+  imovel: {
+    minLoan: 30000.0,
+    maxLoan: 4500000.0,
+    terms: [120, 180, 240]
+  }
+}
